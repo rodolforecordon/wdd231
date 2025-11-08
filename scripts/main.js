ddc776d8ed92e173827a021ff5014c409f673112
Rodolfo Recordon <rodolforecordon@gmail.com>
2025-11-08 22:15:17 +0000

    week01 city chamber home page

---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+// Set current year
+const currentYear = new Date().getFullYear();
+document.getElementById('currentyear').textContent = currentYear;
+
+// Set last modified date
+const lastModified = document.lastModified;
+document.getElementById('lastModified').textContent = `Last Modified: ${lastModified}`;
